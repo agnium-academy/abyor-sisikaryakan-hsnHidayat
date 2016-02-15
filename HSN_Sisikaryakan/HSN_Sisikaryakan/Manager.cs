c044becf7c0cbefe84669f999cedca4e3322af83
hsnHidayat <husain.athfal.hidayat@basajans.com>
2016-02-15 08:30:28 +0700

    Add Subclass Manager.cs
    Adding Subclass Manager.cs which is derived from Employee.cs

---
+++ HSN_Sisikaryakan/HSN_Sisikaryakan/Manager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSN_SisikaryakanHR
+{
+    public class Manager:Employee
+    {
+        protected string department
+        { get; set; }
+
+        public Manager() : base()
+        {
+            this.department = "<tbd>";
+        }
+
+        protected string allowanceType //staff + spv. + THR & Medical
+        { get; set; }        
+
+        protected int allowanceFee
+        { get; set; }
+
+
+        #region Constructor
+        public Manager(string name, string gender, string department) : base()
+        {
+            this.basicSalary = 10000000;
+            this.department = department;
+        }
+
+        public Manager(string name, string gender, string department, string allowanceType, int allowanceFee) : base()
+        {
+            this.basicSalary = 10000000;
+            this.department = department;
+            this.allowanceType = allowanceType;
+            this.allowanceFee = allowanceFee;
+        }
+        #endregion
+    }
+}
