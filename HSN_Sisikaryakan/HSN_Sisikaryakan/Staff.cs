8054f291f1d5d2ac6bb3265d5fd853c6b68283b5
hsnHidayat <husain.athfal.hidayat@basajans.com>
2016-02-15 08:47:38 +0700

    Create Subclass Staff.cs
    Create Subclass Staff.cs which is derived from Employee Class

---
+++ HSN_Sisikaryakan/HSN_Sisikaryakan/Staff.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSN_SisikaryakanHR
+{
+    public class Staff : Employee
+    {
+        protected string department
+        { get; set; }
+
+        protected string location
+        { get; set; }
+
+        protected string training
+        { get; set; }
+
+        protected string assignmentProject
+        { get; set; }
+
+        protected string allowanceType //staff + spv. + THR & Medical
+        { get; set; }
+
+        protected int allowanceFee
+        { get; set; }
+
+
+        #region Constructor
+        public Staff() : base()
+        {
+            this.department = "<tbd>";
+        }
+
+        public Staff(string name, string gender, string department, string location) : base()
+        {
+            this.basicSalary = 4000000;
+            this.department = department;
+            this.location = location;
+        }
+
+        public Staff(string name, string gender, string department, string location, string training, string assignmentProject, string allowanceType, int allowanceFee) : base()
+        {
+            this.basicSalary = 4000000;
+            this.department = department;
+            this.location = location;
+            this.training = training;
+            this.assignmentProject = assignmentProject;
+            this.allowanceType = allowanceType;
+            this.allowanceFee = allowanceFee;
+        }
+        #endregion
+    }
+}
