b290e6a1447f10a4dee8f7280a144d615954c665
hsnHidayat <husain.athfal.hidayat@basajans.com>
2016-02-15 08:28:32 +0700

    Create Initial Project and Base Class
    First Phase by Creating Initial Project and Base Class (Employee.cs)

---
+++ HSN_Sisikaryakan/HSN_Sisikaryakan/Employee.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSN_SisikaryakanHR
+{
+    public class Employee
+    {
+        protected string entryDate
+        { get; set; }
+
+        protected string layoffDate
+        { get; set; }
+
+        protected string employeeStatus
+        { get; set; }
+
+        protected string name
+        { get; set; }
+
+        protected string gender
+        { get; set; }
+
+        protected int basicSalary
+        { get; set; }
+
+        protected int totalSalary
+        { get; set; }
+
+        protected void entryLayoffInfo()
+        {
+            this.entryDate = "February, 15 2016";
+            this.layoffDate = "-";
+            this.employeeStatus = "ACTIVE";
+        }
+
+        protected void entryLayoffInfo(string _entryDate)
+        {
+            this.entryDate = _entryDate;
+        }
+
+        protected void entryLayoffInfo(string _entryDate, string _layoffDate)
+        {
+            this.entryDate = _entryDate;
+            this.layoffDate = _entryDate;
+            this.employeeStatus = "INACTIVE";
+        }
+
+        public Employee()
+        {
+            this.name = "anonymous";
+            this.gender = "?";
+            this.basicSalary = 0;
+        }
+
+        public Employee(string name, string gender)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.basicSalary = 0;
+        }
+
+    }
+}
